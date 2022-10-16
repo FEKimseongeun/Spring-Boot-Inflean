6c4c748b9d8aea486fe6ba7dfea592fd8010887f
LOA(Kimseongeun) <hanhun0318@gmail.com>
2022-10-16 21:23:55 +0900

    ✅ 1016 배운 내용 추가

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,7 +1,7 @@
 package hello.hellospring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class HelloSpringApplication {
--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,7 @@
 package hello.hellospring.controller;
 
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
